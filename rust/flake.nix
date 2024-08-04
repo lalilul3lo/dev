aebeefc355bdbde3ac17527895fd079486d5248e
Samuel Joli <samuel.joli.ftn@gmail.com>
2024-08-04 00:07:24 -0400

    chore: simplify rust flake

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,67 +1,28 @@
+#flake.nix
 {
   description = "A nix flake based Rust development environment.";
 
-  # Flake inputs
-  inputs = {
-    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2311.*.tar.gz";
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  # Flake outputs that other flakes can use
-  outputs = { self, flake-schemas, nixpkgs, rust-overlay }:
-    let
-      # Helpers for producing system-specific outputs
-      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default self.overlays.default ];
+  outputs =
+    { nixpkgs, self }:
+    {
+      packages.aarch64-darwin =
+        let
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        in
+        {
+          default = pkgs.callPackage ./package.nix { };
         };
-      });
-    in {
-      # Schemas tell Nix about the structure of your flake's outputs
-      schemas = flake-schemas.schemas;
-
-      overlays.default = final: prev: {
-        rustToolchain =
-          let
-            rust = prev.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = [ "rust-src" "rustfmt" ];
+      devShells.aarch64-darwin =
+        let
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        in
+        {
+          default = pkgs.mkShell {
+              inputsFrom = [ self.packages.${pkgs.stdenv.system}.default ];
+              packages = [ pkgs.rust-analyzer pkgs.clippy pkgs.rustfmt ];
             };
-      };
-
-      # Development environments
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          # Pinned packages available in the environment
-          packages = with pkgs; [
-            rustToolchain
-            openssl
-            pkg-config
-            cargo-deny
-            cargo-edit
-            cargo-watch
-            rust-analyzer
-          ];
-
-          env = {
-            # Required by rust-analyzer
-            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-          };
         };
-      });
     };
 }
---
+++ rust/package.nix
@@ -0,0 +1,13 @@
+#package.nix
+{ lib, rustPlatform }:
+let
+  toml = (lib.importTOML (./Cargo.toml)).package;
+in
+rustPlatform.buildRustPackage {
+  pname = toml.name;
+  inherit (toml) version;
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+
+  meta.mainProgram = "<binary name>";
+}
